74f899b4ce18f64bcfa787d5bd1a9671b1874408
Yara Castro <yaracastro24@gmail.com>
2019-06-01 11:38:05 -0500

    Add Objecgts answers

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,22 +13,49 @@
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
 // Using your dinosaur objects, log answers to these questions:
+let dinosaurs1 = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  lenght: "12m",
+  period: "Late Cretaceous",
+  roar: function(){
+      return "RAWERSRARARWERSARARARRRR!";
+  }
+};
+
+let dinosaurs2 = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  lenght: "9m",
+  period: "Late Jurassic"
+};
+
+let dinosaurs3 = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  lenght: "1.8m",
+  period: "Late Cretaceous"
+};
+
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaurs1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurs3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurs2.lenght);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaurs1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinosaurs1.roar());
 
 
 // ==== Arrays ====
@@ -60,6 +87,8 @@ Name email@example.com
 Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
+
+
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
